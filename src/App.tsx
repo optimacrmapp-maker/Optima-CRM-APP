c2c06026a449379403f426f737f946cfc4215233
optimacrmapp-maker <optimacrm.app@gmail.com>
2026-03-16 13:09:19 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
+import ResetPassword from './pages/ResetPassword';
 
 function App() {
   return (
-    <BrowserRouter>
+    // O basename avisa ao React que o site roda dentro desta subpasta
+    <BrowserRouter basename="/Optima-CRM-APP">
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
       </Routes>
     </BrowserRouter>
   );
