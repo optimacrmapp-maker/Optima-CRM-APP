58f955264064c6ec7ec1336caec865c699a73d7e
optimacrmapp-maker <optimacrm.app@gmail.com>
2026-03-16 16:48:13 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,8 @@ function App() {
   return (
     <HashRouter>
       <Routes>
-        {/* Rota Principal */}
         <Route path="/" element={<Dashboard />} />
-        
-        {/* Rota de Redefinição de Senha */}
         <Route path="/reset-password" element={<ResetPassword />} />
-
-        {/* Se o link estiver errado, ele sempre volta para o início */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </HashRouter>
