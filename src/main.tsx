89775518bb4732436e6bdd96c81da2bd618210da
optimacrmapp-maker <optimacrm.app@gmail.com>
2026-03-16 13:11:38 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+// REMOVA A LINHA: import './index.css' (se ela estiver aí, o erro continua)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
