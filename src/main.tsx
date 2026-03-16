7f5b75e53ce2e8d4a680e004d313edd31d4d3d5b
optimacrmapp-maker <optimacrm.app@gmail.com>
2026-03-16 15:40:13 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-// Importante: Verifique se você tem o arquivo index.css. 
-// Se não tiver certeza, deixe a linha abaixo comentada com //
-// import './index.css' 
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
