7cc21efdecfa28bcbc27bd6319393013782d8cd3
optimacrmapp-maker <optimacrm.app@gmail.com>
2026-03-16 12:30:17 -0300

    Update App.tsx

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,14 +1,1 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default App;
