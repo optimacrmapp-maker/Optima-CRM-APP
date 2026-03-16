1baeb210e53d8ee8baab87edd1c407ad084d2b27
optimacrmapp-maker <optimacrm.app@gmail.com>
2026-03-16 12:59:33 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Optima-CRM-APP/', // Adicione o nome do seu repositório aqui entre barras
+  base: '/Optima-CRM-APP/', // Isso garante que o site saiba onde encontrar as imagens e scripts
 })
