18b3b0f3eaa3aa6cbc409d90c0549bed5ca7212d
optimacrmapp-maker <optimacrm.app@gmail.com>
2026-03-16 17:01:03 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
-import ResetPassword from './pages/ResetPassword';
 
 function App() {
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
+        {/* Redireciona qualquer erro para o Dashboard */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </HashRouter>
